20266702ee87158b00b982e3fe598532cac3904a
PadowYT2 <me@padow.ru>
2023-11-24 21:11:14 +0300

    Remove useless folders

--- examples/fileInput/index.ts
+++
@@ -1,5 +0,0 @@
-export { root } from './fileInput.root';
-export { helper } from './fileInput.helper';
-export { multiple } from './fileInput.multiple';
-export { sizes } from './fileInput.sizes';
-export { dropzone } from './fileInput.dropzone';
