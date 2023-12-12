1b36ef9ebe5aa389e885603c8037f94f74f531f0
PadowYT2 <me@padow.ru>
2023-12-12 19:34:27 +0300

    import React

--- src/components/Banner/Banner.spec.tsx
+++ src/components/Banner/Banner.spec.tsx
@@ -1,7 +1,8 @@
-import userEvent from '@testing-library/user-event';
-import { Banner } from './Banner';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
 import { describe, expect, it } from 'vitest';
+import { Banner } from './Banner';
 
 describe('Components / Banner', () => {
   it('should close when collapse button is clicked', async () => {
